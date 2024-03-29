1cb4c0c790b52e018627bf09ade0576ab602efa6
dvlpr2003 <gayathrigaya698@gmail.com>
2024-03-29 21:12:00 +0530

    officer-page

--- src/officer/officer.jsx
+++ src/officer/officer.jsx
@@ -42,25 +42,33 @@ export default function OfficerPage(){
     }
     GetUser()
 },[])
+async function GetComplaintData(){
+    try{
+        const response = await axios.get("http://127.0.0.1:8000/api/complaints/");
+        console.log(response.data)
+    }catch(error){
+        console.log(error)
+    }
+}
     
     return(
         <div id="officer-main-page">
-        <OfficerOption totalComplaint={totalComplaint} totalUser={totalUser} />
+        <OfficerOption totalComplaint={totalComplaint} totalUser={totalUser} GetComplaintData={GetComplaintData} />
         <QueryPage UserList={UserList} setOverData={setOverData}/>
         <UserOverview OverData={OverData}/>
 
         </div>
     )
 }
-function OfficerOption({totalComplaint,totalUser}){
+function OfficerOption({totalComplaint,totalUser,GetComplaintData}){
 
     return(
         <div id="officer-options">
-            <OfficerItems totalUser={totalUser} totalComplaint={totalComplaint}/>
+            <OfficerItems totalUser={totalUser} totalComplaint={totalComplaint} GetComplaintData={GetComplaintData}/>
         </div>
     )
 }
-function OfficerItems({totalUser,totalComplaint}){
+function OfficerItems({totalUser,totalComplaint,GetComplaintData}){
     return(
         <div id="officer-item-container">
             <div className="officer-item-container-options">
@@ -79,7 +87,7 @@ function OfficerItems({totalUser,totalComplaint}){
                 </div>
                 
             </div>
-            <div className="officer-item-container-options">
+            <div className="officer-item-container-options" onClick={GetComplaintData}>
                 <div className="officer-svg">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="officer-user-svg">
   <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
@@ -107,10 +115,12 @@ function OfficerItems({totalUser,totalComplaint}){
 function QueryPage({UserList,setOverData}){
     return(
         <div id="officer-Query-page">
-            {
+            {/* {
             
-            UserList && <RegisteredUser UserList={UserList} setOverData={setOverData}/>
-            }
+            <RegisteredUser UserList={UserList} setOverData={setOverData}/>
+            } */}
+            <ComplaintsDetails/>
+
 
         </div>
     )
@@ -120,7 +130,7 @@ function RegisteredUser({UserList,setOverData}){
     return(
         <div id="Registered-user-main">
             {
-                UserList.map((e)=><RegisteredUserItems 
+                UserList && UserList.map((e)=><RegisteredUserItems 
                 fname={e.fname}
                  lname={e.lname} 
                  address={e.address} 
@@ -134,6 +144,35 @@ function RegisteredUser({UserList,setOverData}){
         </div>
     )
 }
+function ComplaintsDetails(){
+    return(
+        <div style={{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center",gap:"2rem"}}>
+            <ComplaintDetailsItems/>
+            <ComplaintDetailsItems/>
+        </div>
+    )
+}
+
+function ComplaintDetailsItems(){
+    return(
+        <div className="Complaint-Details-overview-point">
+            <div className="complaint-details-content">
+                <h2>myComplaints heading</h2>
+                
+                    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, totam laboriosam optio est quaerat vel eius tempore doloribus! Nisi et earum nobis excepturi saepe enim dolores architecto, quas nihil deleniti.
+                    </span>
+                
+            </div>
+            <div className="complaint-details-content-image">
+                <img src="img/kid_login.jpeg"/>
+
+            </div>
+            
+        </div>
+    )
+}
+
+
 function RegisteredUserItems({fname,lname,address,district,mail,phone,pincode,setOverData}){
     function updatelist(){
         setOverData()
