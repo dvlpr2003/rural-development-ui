2526369f4c8aa91ecf9ba78cb9c2b66471d1b9ff
dvlpr2003 <gayathrigaya698@gmail.com>
2024-04-14 11:05:41 +0530

    responsiveness

--- src/footer/footer.jsx
+++ src/footer/footer.jsx
@@ -2,7 +2,8 @@
 import { NavLink } from "react-router-dom"
 import "./footer.css"
 export default function Footer(){
-    return(<div id="footer-main">
+    return(
+    <div id="footer-main">
         <div className="footer-containers" id="foo-heading-container">
             <div id="footer-heading">
             <span style={{color:"black"}}>Rural</span>
@@ -10,7 +11,7 @@ export default function Footer(){
             </div>
             <div id="footer-contact">
                 <div>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" /></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6" id="phone-icon"><path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" /></svg>
 
                 </div>
                 <span>+123456789</span>
@@ -41,6 +42,7 @@ export default function Footer(){
             </div>
             
         </div>
+
         <div className="footer-containers">
             <span className="rs-foo-head">Login Info</span>
             <div id="people-login">
@@ -57,8 +59,8 @@ export default function Footer(){
 
         </div>
         <div className="footer-containers">
-            <div>
-                <h2>Follow Us</h2>
+            <div id="follow-us-main">
+                <h2 id="followus">Follow Us</h2>
                 <div id="instagram-profiles">
                     <NavLink to="https://www.instagram.com/kpashokkumar08/" target="new">
                     <div>
