0e79bacbe7c2a3c861574325a9b7bd14660677b8
dvlpr2003 <gayathrigaya698@gmail.com>
2024-04-25 11:14:52 +0530

    responsive

--- src/Home/container-3/Container3.jsx
+++ src/Home/container-3/Container3.jsx
@@ -33,14 +33,21 @@ function Container3Heading(){
 function Container3Para(){
     return(
         <div id="container3para-container">
-            <span id="container3para">
+            <p>
+            Agricultural development stands as the cornerstone of rural prosperity in Tamil Nadu, where the fertile lands and rich 
+            traditions intertwine to shape the agricultural landscape. Through innovative practices, technological advancements, and
+            strategic interventions, agricultural development initiatives aim to enhance productivity, improve livelihoods, and ensure food
+            security for rural communities. Join us as we delve into the multifaceted realm of agricultural development, where the seeds of
+            progress sow the promise of a sustainable and thriving future for Tamil Nadu's agrarian heartland.
+            </p>
+            {/* <span id="container3para">
                 Agricultural development stands as the cornerstone of rural prosperity in Tamil Nadu, where the fertile lands and rich 
 
             </span>
             <span>traditions intertwine to shape the agricultural landscape. Through innovative practices, technological advancements, and </span>
             <span>strategic interventions, agricultural development initiatives aim to enhance productivity, improve livelihoods, and ensure food </span>
             <span>security for rural communities. Join us as we delve into the multifaceted realm of agricultural development, where the seeds of</span>
-            <span>progress sow the promise of a sustainable and thriving future for Tamil Nadu's agrarian heartland.</span>
+            <span>progress sow the promise of a sustainable and thriving future for Tamil Nadu's agrarian heartland.</span> */}
             <NavLink to="/Agriculture-and-Development"><span>Learn more...</span></NavLink>
         </div>
     )
