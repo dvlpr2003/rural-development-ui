c473c28c7883b5e14b8b5aed83184d65a7097727
dvlpr2003 <gayathrigaya698@gmail.com>
2024-05-07 10:16:10 +0530

    -r

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -101,7 +101,7 @@ export default function SignUp({isLogin}){
                 }
                 </div>
             </div>
-            {/* { isVerify ?
+            { isVerify ?
             <OTPVerifyPage
             setOTP1={setOTP1}
             setOTP2={setOTP2}
@@ -118,8 +118,8 @@ export default function SignUp({isLogin}){
             Verify_Otp={Verify_Otp}
 
             />
- :""} */}
- <OTPVerifyPage/>
+ :""}
+ {/* <OTPVerifyPage/> */}
         </div>
         </>
     )
